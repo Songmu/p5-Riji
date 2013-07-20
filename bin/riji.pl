d0050ec1969197b1819cf6a23dea73d5bea91a32
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-20 16:13:26 +0900

    migrate to Punchuer

---
+++ bin/riji.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+
+use Puncheur::Runner;
+
+Puncheur::Runner->new('Riji')->run;
--- lib/Riji.pm
+++ lib/Riji.pm
@@ -1,10 +1,27 @@
 package Riji;
-
-use strict;
-use warnings;
-use utf8;
+use Puncheur::Lite;
+use Path::Tiny;
 
 our $VERSION = 0.01;
 
-1;
+__PACKAGE__->setting(
+    handle_static => 1,
+);
 
+get '/' => sub {
+    my $c = shift;
+    $c->render('index.tx', { greeting => "Hello" });
+};
+
+get '/entry/:name' => sub {
+    my ($c, $args) = @_;
+
+    my $name = $args->{name};
+
+    my $md_file = path($c->base_dir)->child('docs', '2013020201.md');
+    my $article = $md_file->slurp;
+
+    $c->create_response(200, ['Content-Type' => 'text/plain'], [$article]);
+};
+
+1;
