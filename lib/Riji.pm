799273577ef17ba3dc5995e0f719cf9a8a4f1397
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-20 16:43:53 +0900

    update

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -1,6 +1,7 @@
 package Riji;
 use Puncheur::Lite;
 use Path::Tiny;
+use Text::Markdown::Discount;
 
 our $VERSION = 0.01;
 
@@ -13,15 +14,24 @@ get '/' => sub {
     $c->render('index.tx', { greeting => "Hello" });
 };
 
-get '/entry/:name' => sub {
+get '/entry/:name.html' => sub {
     my ($c, $args) = @_;
 
     my $name = $args->{name};
+    return $c->res_404 if $name =~ /[^-_.a-zA-Z0-9]/;
 
-    my $md_file = path($c->base_dir)->child('docs', '2013020201.md');
+    my $md_file = path($c->base_dir)->child('docs',  "$name.md");
+
+    return $c->res_404 unless -f $md_file;
+
     my $article = $md_file->slurp;
 
-    $c->create_response(200, ['Content-Type' => 'text/plain'], [$article]);
+    state $md = Text::Markdown::Discount->new;
+    $article = $md->markdown($article);
+
+    $c->render('entry.tx', {
+        article => $article,
+    });
 };
 
 1;
