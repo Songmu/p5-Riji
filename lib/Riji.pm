b84d1eb414afcbdd895eae935d7ac30ce29ba2c6
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-24 17:00:34 +0900

    preprocessing feature [EXPERIMENTAL]

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -2,6 +2,7 @@ package Riji;
 use Puncheur::Lite;
 
 use Encode;
+use Text::Xslate;
 
 our $VERSION = 0.01;
 
@@ -40,12 +41,27 @@ get '/entry/{name:[-_a-zA-Z0-9]+}.html' => sub {
     my $entry = $blog->entry($name);
     return $c->res_404 unless $entry;
 
+    my $xslate = Text::Xslate->new(
+        type => 'text',
+        function => {
+            c         => sub { $c },
+            uri_for   => sub { $c->uri_for(@_) },
+        }
+    );
+    my $body = $xslate->render_string($entry->body, {
+        entry => $entry,
+        blog  => $blog,
+    });
+    $entry->body($body);
+    my $article = $entry->body_as_html;
+
     my $tmpl = $entry->template // 'entry';
     $tmpl .= '.tx';
 
     $c->render($tmpl, {
-        blog  => $blog,
-        entry => $entry,
+        blog    => $blog,
+        entry   => $entry,
+        article => $article,
     });
 };
 
