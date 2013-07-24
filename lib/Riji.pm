b0806951410d2f537d1f6cf5f33c48097a95b65f
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-24 16:21:07 +0900

    use plugin ShareDir

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -8,7 +8,7 @@ our $VERSION = 0.01;
 __PACKAGE__->setting(
     handle_static => 1,
 );
-__PACKAGE__->load_plugin(qw/Model/);
+__PACKAGE__->load_plugin(qw/Model ShareDir/);
 
 get '/{page:(?:[-_a-zA-Z0-9]+.html)?}' => sub {
     my ($c, $args) = @_;
