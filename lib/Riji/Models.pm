d89b6578407c3f843f1f7326a57006b22837acba
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-29 00:59:08 +0900

    update site_url sovling

--- lib/Riji/Models.pm
+++ lib/Riji/Models.pm
@@ -9,9 +9,16 @@ use Object::Container -base;
 register Blog => sub {
     my $self = shift;
     my $conf = $self->get('config');
+    my $site_url = sub {
+        return URI->new($conf->{site_url}) if $conf->{site_url};
+
+        require Riji;
+        Riji->context ? Riji->context->req->base : URI->new('http://unknown.example.com/');
+    }->();
+
     $self->ensure_class_loaded('Riji::Model::Blog')->new(
         base_dir => $self->get('base_dir'),
-        site_url => URI->new($conf->{site_url}),
+        site_url => $site_url,
         author   => $conf->{author},
         title    => $conf->{title},
     );
