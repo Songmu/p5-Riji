4bc3d3eb44b2bcd0e822a0b3ffd84059b9398144
Songmu <y.songmu@gmail.com>
2014-12-26 15:39:57 +0900

    Checking in changes prior to tagging of version v0.2.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 428b382..1306ef5 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Riji
    
     {{$NEXT}}
    
    +v0.2.0 2014-12-26T06:39:52Z
    +
    +    - [Experimental][feature] riji publish --force (trapple++)
    +    - change version declaration way
    +
     0.1.3 2014-08-06T10:15:59Z
    
         - `git symbolic-ref --short` is available after git 1.7.10, so care older version when publishing.

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -7,7 +7,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.1.3");
+use version 0.77; our $VERSION = version->declare("v0.2.0");
 
 __PACKAGE__->setting(
     handle_static => 1,
