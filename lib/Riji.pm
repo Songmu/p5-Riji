45481ab6b7251529c873bf0a5fc584a0e6a9fcec
Songmu <y.songmu@gmail.com>
2015-01-02 16:34:59 +0900

    Checking in changes prior to tagging of version v0.3.1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e70ae88..b9718d8 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Riji
    
     {{$NEXT}}
    
    +v0.3.1 2015-01-02T07:34:52Z
    +
    +    - [bugfix] care relative links
    +
     v0.3.0 2015-01-01T18:53:51Z
    
         - depends on Puncheur v0.3.0 or later

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -7,7 +7,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.3.0");
+use version 0.77; our $VERSION = version->declare("v0.3.1");
 
 __PACKAGE__->setting(
     handle_static => 1,
