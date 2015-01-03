41f992579235b267bd1286f5846aad963a413c25
Songmu <y.songmu@gmail.com>
2015-01-04 00:43:44 +0900

    Checking in changes prior to tagging of version v0.4.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 75d9914..c7a768a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,10 @@ Revision history for Perl extension Riji
    
     {{$NEXT}}
    
    +v0.4.0 2015-01-03T15:43:38Z
    +
         - `git publish` is prohibited only when `entry_dir` is dirty index, not all files in repository
    +    - entry directory can have subdirectories now
    
     v0.3.2 2015-01-02T15:41:48Z

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -7,7 +7,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.3.2");
+use version 0.77; our $VERSION = version->declare("v0.4.0");
 
 __PACKAGE__->setting(
     handle_static => 1,
