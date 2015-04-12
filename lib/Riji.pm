0c0e0e7bb5e57eb71b7c8ebc12bc38f66782515f
Songmu <y.songmu@gmail.com>
2015-04-12 21:56:01 +0900

    Checking in changes prior to tagging of version v0.9.7.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a39d5d9..b0c60be 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Riji
    
     {{$NEXT}}
    
    +v0.9.7 2015-04-12T12:55:49Z
    +
         - support Front-matter
    
     v0.9.6 2015-04-11T17:07:51Z

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -8,7 +8,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.9.6");
+use version 0.77; our $VERSION = version->declare("v0.9.7");
 
 __PACKAGE__->setting(
     handle_static => 1,
