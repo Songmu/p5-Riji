861304582210bc308ca67319e76473ed2deca1cf
Songmu <y.songmu@gmail.com>
2022-01-02 03:02:16 +0900

    Checking in changes prior to tagging of version v0.9.11.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 360bc0e..49ce459 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Riji
     
     {{$NEXT}}
    +
    +v0.9.11 2022-01-01T18:01:53Z
         - Add Dockerfile
         - In riji publish, take care of the case where the host of the target URL is undefined

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -8,7 +8,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.9.10");
+use version 0.77; our $VERSION = version->declare("v0.9.11");
 
 __PACKAGE__->setting(
     handle_static => 1,
