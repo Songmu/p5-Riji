e27784a3e80aac7fbfedb2865be2505e29b70078
Songmu <y.songmu@gmail.com>
2022-01-02 15:41:55 +0900

    Checking in changes prior to tagging of version 1.0.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d32cf14..fc5c700 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Riji
     
     {{$NEXT}}
     
    +1.0.0 2022-01-02T06:41:09Z
    +    - v1 grand release
    +
     v0.9.15 2022-01-02T06:25:28Z
         - No code changes
         - Enhance documents

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -8,7 +8,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v0.9.15");
+use version 0.77; our $VERSION = version->declare("1.0.0");
 
 __PACKAGE__->setting(
     handle_static => 1,
