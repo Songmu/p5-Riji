92d80eecb9a0238a7e2da7db17f4aedfcd4f1447
Songmu <y.songmu@gmail.com>
2024-10-30 20:42:27 +0900

    Checking in changes prior to tagging of version v1.1.1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a3d21eb..e97fcf6 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Riji
     
     {{$NEXT}}
    +
    +v1.1.1 2024-10-30T11:42:16Z
         - use feature ':5.20' instead of use 5.010 for deprecated::subsequent_use_version
     
     v1.1.0 2024-10-29T11:29:04Z

--- lib/Riji.pm
+++ lib/Riji.pm
@@ -8,7 +8,7 @@ use Encode;
 use File::Spec;
 use YAML::Tiny ();
 
-use version 0.77; our $VERSION = version->declare("v1.1.0");
+use version 0.77; our $VERSION = version->declare("v1.1.1");
 
 __PACKAGE__->setting(
     handle_static => 1,
